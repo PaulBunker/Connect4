584056aba824700fadd04ce8119e38687a97ad88
Paul Bunker <paulmarkbunker@gmail.com>
2019-02-28 07:13:48 +0000

    added some testing

--- src/js/components/board/board.jsx
+++ src/js/components/board/board.jsx
@@ -4,9 +4,9 @@ import Column from '../column/column.jsx'
 const RED = 'r'
 const YELLOW = 'y'
 
-const create2DArray = (cols = 5, rows = 5) => [...Array(cols)].map(() => Array(rows).fill(null))
+export const create2DArray = (cols = 5, rows = 5) => [...Array(cols)].map(() => Array(rows).fill(null))
 
-const addChecker = (gameState, column, colour) => {
+const addChequer = (gameState, column, colour) => {
   const newGameState = [...gameState]
   let activeColumn = newGameState[column]
   let done
@@ -21,8 +21,49 @@ const addChecker = (gameState, column, colour) => {
   return newGameState
 }
 
+export const check4InARow = (array) => {
+  let winner = false
+  let count = 0
+  let potentialWinner
+  array.forEach(item => {
+    if(potentialWinner !== item || item === null) {
+      count = 1
+      potentialWinner = item
+    } else {
+      count += 1
+    }
+    if (count === 4) {
+      winner = potentialWinner
+    }
+  })
+  return winner
+}
+
+const checkColumns = (gameState) => {
+  gameState.some(column => {
+    winner = check4InARow(column)
+    if(winner){
+      return winner
+    }
+  });
+}
+
+const checkRows = (gameState) => {
+  gamesState[0].some((_,rowNumber) => {
+    winner = check4InARow(gamestate.map(column => column[rowNumber]))
+    if(winner){
+      return winner
+    }
+  })
+}
+
+const checkForAWin = (gameState) => {
+  columnWinner = checkColumns(gameState) 
+  rowWinner = checkRows(gameState)
+}
+
 const initialGameState = create2DArray()
-const state1 = addChecker(initialGameState, 0, RED)
+
 
 
 
