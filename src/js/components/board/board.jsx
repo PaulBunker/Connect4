564c75901e9100023196344f0c9ec5a6822a5e22
Paul Bunker <paulmarkbunker@gmail.com>
2019-03-04 23:19:43 +0000

    ugh

--- src/js/components/board/board.jsx
+++ src/js/components/board/board.jsx
@@ -9,18 +9,51 @@ const YELLOW = 'y'
 
 export const create2DArray = (cols = 7, rows = 7) => [...Array(cols)].map(() => Array(rows).fill(null))
 
+const minItem = data => data.reduce((a, b) => a.value <= b.value ? a : b, {})
+const maxItem = data => data.reduce((a, b) => a.value >= b.value ? a : b, {})
+
 const minimax = (board, depth, player) => {
   const didWin = checkForAWin(board)
 
-  if (!didWin) {
+  if (didWin === null) {
+    const values = []
 
+    for (let columnNumber = 0; columnNumber < board.length; columnNumber++) {
+      const column = board[columnNumber]
+      if (column[column.length - 1] === null) {
+        const newGrid = addChequer(board, columnNumber, player)
+        const value = minimax(newGrid, depth + 1, (player === RED) ? YELLOW : RED)
+        values.push({
+          column,
+          value,
+        })
+      }
+    }
+
+    if (player === RED) {
+      const { column, value } = maxItem(values)
+      if (depth === 0) {
+        return column
+      }
+      return value
+    }
+    const { column, value } = minItem(values)
+    if (depth === 0) {
+      return column
+    }
+    return value
+  } if (didWin === YELLOW) {
+    return depth - 9999
+  } if (didWin === RED) {
+    return 9999 - depth
   }
-
+  return 0
 }
 
 class Board extends Component {
   constructor() {
     super()
+    this.i = 2
     this.state = {
       board: create2DArray(),
       player: RED,
@@ -35,8 +68,22 @@ class Board extends Component {
       board: newBoard,
       player: player === RED ? YELLOW : RED,
       winner: didWin,
-    })
+    }, this.computerMove)
   };
+
+  computerMove() {
+    const { board, player, i } = this.state
+    this.i++
+    if (player === YELLOW) {
+      const newBoard = addChequer(board, this.i, player)
+      const didWin = checkForAWin(newBoard)
+      this.setState({
+        board: newBoard,
+        player: player === RED ? YELLOW : RED,
+        winner: didWin,
+      })
+    }
+  }
 
   render() {
     const { board, winner } = this.state
