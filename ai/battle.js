07db8a6b8e423f05b7981ec0f6606599570f9e85
Paul Bunker <paulmarkbunker@gmail.com>
2019-03-11 07:08:36 +0000

    test machine learning

--- ai/battle.js
+++ ai/battle.js
@@ -6,24 +6,65 @@ import addChequer from '../src/js/helpers/addChequer/addChequer'
 import minimax from '../src/js/helpers/minimax/minimax'
 import { checkForAWin } from '../src/js/helpers/checkWin/checkWin'
 
+const getMove = (player, gameState, model) => {
+  if (player === 'y') {
+    const preds = model.predict(tf.tensor([initialState()])).dataSync()
+    const list = []
+    preds.forEach((pred, i) => {
+      list.push({
+        column: i,
+        pred,
+      })
+    })
+    const orderedPredictions = [...list].sort((a, b) => {
+      if (a.pred < b.pred) { return 1 }
+      if (a.pred > b.pred) { return -1 }
+      return 0
+    })
+    for (let index = 0; index < orderedPredictions.length; index++) {
+      const prediction = orderedPredictions[index]
+      if (gameState[prediction.column].indexOf(null) !== -1) {
+        return prediction.column
+      }
+    }
+  }
+  return minimax(gameState, 0, player, -10000, 10000, 3)
+}
+
 const battle = (model) => {
   const games = {
     modelWins: 0,
     minimaxWins: 0,
-    drawWins: 0,
+    draws: 0,
   }
   let initialPlayer = 'y'
-  for (let game = 0; game < 1; game++) {
+
+  for (let game = 0; game < 10000; game++) {
     let playing = true
     initialPlayer = initialPlayer === 'y' ? 'r' : 'y'
-    const player = initialPlayer
-    const gameState = initialState()
+    let player = initialPlayer
+    let gameState = initialState()
     while (playing) {
-      const column = model.predict(tf.tensor([gameState])).argMax([-1]).dataSync()[0]
-      console.log(column)
-      playing = false
+      const column = getMove(player, gameState, model)
+      const newGamestate = addChequer(gameState, column, player)
+      const didWin = checkForAWin(newGamestate)
+      if (didWin || didWin === false) {
+        if (didWin === 'y') {
+          games.modelWins++
+        } else if (didWin === 'r') {
+          games.minimaxWins++
+        } else {
+          games.draws++
+        }
+        console.log(newGamestate)
+        playing = false
+      } else {
+        gameState = newGamestate
+        player = player === 'y' ? 'r' : 'y'
+      }
     }
   }
+  console.log(games)
 }
 
 tf.loadLayersModel(`file://${path.join(__dirname, 'models', 'model.json')}`).then((model) => {
